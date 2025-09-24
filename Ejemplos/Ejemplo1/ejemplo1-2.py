112b15ecafcaaa151c9f28588fafc6d2a82ec2f3
Ismael Abdeslam Zaim Wadghiri Amnad <izaiamn854@g.educaand.es>
2025-09-24 13:49:33 +0200

    tipos de variable

--- Ejemplos/Ejemplo1/ejemplo1-2.py
+++ Ejemplos/Ejemplo1/ejemplo1-2.py
@@ -1,3 +1,4 @@
-nombre = "Ismael"
-edad = 29
-print(f"Hola, {nombre}. Tienes {edad} años.")
+nombre = "Ismael" # Variable de cadena de texto
+edad = 29 # Variable entera
+precio = 19.99 # Variable de punto flotante
+test = True # Variable booleana
