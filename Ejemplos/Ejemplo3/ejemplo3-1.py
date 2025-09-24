e66fa9fec6590fac43334bafb5a045f4b23907d5
Ismael Abdeslam Zaim Wadghiri Amnad <izaiamn854@g.educaand.es>
2025-09-24 14:35:38 +0200

    char to num

--- Ejemplos/Ejemplo3/ejemplo3-1.py
+++ Ejemplos/Ejemplo3/ejemplo3-1.py
@@ -3,5 +3,5 @@ nombre = "Ismael" # Variable de cadena de texto
 edad = 29 # Variable entera
 precio = 19.99 # Variable de punto flotante
 test = True # Variable booleana
-
-print("Hola")
+nombre = input("Introduce tu nombre: ")
+print("Hola " + nombre)
---
+++ Ejemplos/Ejemplo3/ejemplo3-3.py
@@ -0,0 +1,4 @@
+num = input("Introduce un número: ")
+num = int(num) # Convertir a entero
+num *= 2
+print("El doble es: " + str(num))
