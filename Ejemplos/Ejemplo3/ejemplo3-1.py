13a8f7a66c0318377e75badc31de07ac53f8c58b
Ismael Abdeslam Zaim Wadghiri Amnad <izaiamn854@g.educaand.es>
2025-09-24 14:19:46 +0200

    nuevo documento y rename

---
+++ Ejemplos/Ejemplo3/ejemplo3-1.py
@@ -0,0 +1,7 @@
+#Ejemplo de variables en Python
+nombre = "Ismael" # Variable de cadena de texto
+edad = 29 # Variable entera
+precio = 19.99 # Variable de punto flotante
+test = True # Variable booleana
+
+print("Hola")
