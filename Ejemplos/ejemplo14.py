00c739d4d75cd8f6ba036090735872b41fdaef37
Ismael Abdeslam Zaim Wadghiri Amnad <izaiamn854@g.educaand.es>
2025-10-29 14:44:59 +0100

    juego final de 29

--- Ejemplos/ejemplo14.py
+++ Ejemplos/ejemplo14.py
@@ -1,11 +1,24 @@
 import random
 import time
+max = 10
+min = 1
+contador = 0
+numero_secreto = random.randint(1, 10)
+time.sleep(1)
+
+print("¡Comienza el juego! La maquina tiene 3 intento para adivinar el número secreto entre 1 y 10.")
 
 for i in range(3):
-    numero_aleatorio = random.randint(1, 10)
+    numero_aleatorio = random.randint(min, max)
     time.sleep(1)
-    if numero_aleatorio == 5:
-        print("¡Has ganado!")
+    if numero_aleatorio == numero_secreto:
+        print("¡Has ganado! El número secreto era:", numero_secreto)
         break
-    else:
-        print(f"Número generado: {numero_aleatorio}. Inténtalo de nuevo.")
+    elif numero_aleatorio > numero_secreto:
+        print(f"Fallo el numero era {numero_aleatorio}. Numero de intentos {contador + 1}. El numero secreto es menor.")
+        max = numero_aleatorio - 1
+    elif numero_aleatorio < numero_secreto:
+        print(f"Fallo el numero era {numero_aleatorio}. Numero de intentos {contador + 1}. El numero secreto es mayor.")
+        min = numero_aleatorio + 1
+else:
+    print("¡Has perdido! El número secreto era:", numero_secreto)
